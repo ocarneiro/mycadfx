ac70af3684e51b6948d89a3a7455ae1cea3e46c9
Otavio Carneiro <git@carneiro.blog.br>
2023-05-28 11:15:07 -0300

    Basic mouse interaction

--- src/main/java/br/blog/carneiro/mycadfx/MyCadMainController.java
+++ src/main/java/br/blog/carneiro/mycadfx/MyCadMainController.java
@@ -4,11 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import lombok.extern.java.Log;
 
 @Log
 public class MyCadMainController {
+
+    private boolean cursorOn = false;
 
     @FXML
     private Canvas mainCanvas;
@@ -22,6 +25,20 @@ public class MyCadMainController {
             GraphicsContext gc = mainCanvas.getGraphicsContext2D();
             gc.setFill(Color.BLUE);
             gc.fillRect(75, 75, 100, 100);
+        }
+        if (key.equalsIgnoreCase("L")) {
+            cursorOn = true;
+        } else {
+            cursorOn = false;
+        }
+    }
+
+    @FXML
+    public void onMouseMoved(MouseEvent mouseEvent) {
+        if(cursorOn) {
+            GraphicsContext gc = mainCanvas.getGraphicsContext2D();
+            gc.setFill(Color.BLUE);
+            gc.fillOval(mouseEvent.getX(), mouseEvent.getY(), 100, 100);
         }
     }
 }
