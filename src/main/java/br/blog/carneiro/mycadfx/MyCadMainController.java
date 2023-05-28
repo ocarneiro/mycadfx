d6790cb5a53aab5a32253163300b374e3db7cc57
Otavio Carneiro <git@carneiro.blog.br>
2023-05-28 02:16:44 -0300

    Adds lombok for logging

--- src/main/java/br/blog/carneiro/mycadfx/MyCadMainController.java
+++ src/main/java/br/blog/carneiro/mycadfx/MyCadMainController.java
@@ -5,7 +5,9 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
+import lombok.extern.java.Log;
 
+@Log
 public class MyCadMainController {
 
     @FXML
@@ -13,9 +15,13 @@ public class MyCadMainController {
 
     @FXML
     public void onKeyReleased(KeyEvent keyEvent) {
-        GraphicsContext gc = mainCanvas.getGraphicsContext2D();
-
-        gc.setFill(Color.BLUE);
-        gc.fillRect(75,75,100,100);
+        String key = keyEvent.getText();
+        log.warning(key);
+        // TODO: replace with action mapping
+        if (key.equalsIgnoreCase("R")) {
+            GraphicsContext gc = mainCanvas.getGraphicsContext2D();
+            gc.setFill(Color.BLUE);
+            gc.fillRect(75, 75, 100, 100);
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module br.blog.carneiro.mycadfx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires java.logging;
+    requires lombok;
+
     opens br.blog.carneiro.mycadfx to javafx.fxml;
     exports br.blog.carneiro.mycadfx;
 }
